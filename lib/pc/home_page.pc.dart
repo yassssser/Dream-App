cec5326f8f11889a12e5b7e2c705f8fd505f3884
Yasser Benmman <yasserbenmman@gmail.com>
2023-01-29 02:03:36 +0100

    change bg image

--- lib/pc/home_page.pc.dart
+++ lib/pc/home_page.pc.dart
@@ -17,7 +17,7 @@ class HomePagePc extends StatelessWidget {
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/images/bg.png"),
-            fit: BoxFit.fill,
+            fit: BoxFit.cover,
           ),
         ),
         child: ListView(
